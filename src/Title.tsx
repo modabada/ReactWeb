c70219e41413f0c83db5bc9e26f3195eb2f10647
문인우 <miw1102@naver.com>
2021-12-01 22:31:55 +0900

    타이핑 효과 세분화,

--- src/Title.tsx
+++ src/Title.tsx
@@ -1,33 +1,58 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Title.css';
 
-
-const word = "1인 청소년 웹개발\n연습 홈페이지"
-
 function Title() {
+    const word = " 1인 청소년 웹개발 연습 홈페이지".normalize("NFKD");
+    const dict: Function = (n: string) => {
+        switch (n) {
+            case "ᄀ": return "ㄱ";
+            case "ᄂ": return "ㄴ";
+            case "ᄃ": return "ㄷ";
+            case "ᄅ": return "ㄹ";
+            case "ᄆ": return "ㅁ";
+            case "ᄇ": return "ㅂ";
+            case "ᄉ": return "ㅅ";
+            case "ᄋ": return "ㅇ";
+            case "ᄌ": return "ㅈ";
+            case "ᄎ": return "ㅊ";
+            case "ᄏ": return "ㅋ";
+            case "ᄐ": return "ㅌ";
+            case "ᄑ": return "ㅍ";
+            case "ᄒ": return "ㅎ";
+            default: return n;
+        }
+    }
+    const word_output = () => {
+        let code = word[index].charCodeAt(0);
+        if(4352 <= code && code <= 4370) {
+            return word.slice(0, index) + dict(word[index])
+        }
+        else {
+            return word.slice(0, index + 1);
+        }
+    }
+
     const timer = useRef(0);
     const interv: any = useRef(0);
-    const [index, SetIndex] = useState(0);
-    console.log(timer.current);
-    useEffect( () => {
-        interv.current = setInterval( () => {
-            if(timer.current++ > 4) {
-                SetIndex(timer.current - 5);
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+        interv.current = setInterval(() => {
+            if (timer.current++ > 4) {
+                setIndex(timer.current - 5);
             }
-        }, 200);
+        }, 130);
         return () => clearInterval(interv.current);
     }, []);
-    useEffect( () => {
-        if(index > 25) {
+    useEffect(() => {
+        if (index > 38) {
             clearInterval(interv.current);
         }
     }, [index]);
 
-
     return (
         <div className="Title">
-            <h1 className="MainTitle">문 인 우</h1> 
-            <h5 className="SubTitle">{word.slice(0, index)}</h5>
+            <h1 className="MainTitle">문 인 우</h1>
+            <h5 className="SubTitle">{word_output()} </h5>
         </div>
     )
 }
