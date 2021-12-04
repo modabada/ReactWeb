bd04fdcd3eeb6a12f7a7a9908c02773c3c211566
문인우 <miw1102@naver.com>
2021-12-04 21:56:50 +0900

    버튼 div로 대체, 클릭이벤트 연구

--- src/MenuBar.tsx
+++ src/MenuBar.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import './MenuBar.css';
 
 function MenuBar() {
@@ -13,8 +12,36 @@ function MenuBar() {
     )
 }
 
+
+class button {
+    prop;
+    constructor(prop: any) {
+        this.prop = prop;
+    }
+    static test1(e: number) {
+        alert(e);
+    }
+
+    static Section(prop: any) {
+        return(<div className="Btn" onClick={this.test1.bind(this, prop.index)}>난버튼{prop.index}</div>)
+    }
+}
+const moveLink = (e: number) => {
+    switch(e) {
+        case 0:
+            alert("ㅎㅇ");
+            break;
+    }
+}
+
+function test() {
+    alert("test");
+}
+function test1(e: number) {
+    alert("t" + e);
+}
 function Section(prop: any) {
-    return(<h1>{prop.index}</h1>)
+    return(<div className="Btn" onClick={(e) => test1.bind(e, prop.index)}>난 버튼{prop.index}</div>)
 }
 
 export default MenuBar;
